b07d985749eeb6e2fc0b67973e93db1a4b9c183a
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-02-11 16:11:51 -0500

    Second Assignment proyect

---
+++ setGame/setGame/SetCard.swift
@@ -0,0 +1,19 @@
+//
+//  SetCard.swift
+//  setGame
+//
+//  Created by Daniel Bermudez on 2/11/19.
+//  Copyright © 2019 Endava. All rights reserved.
+//
+
+import Foundation
+struct setCard{
+   var shape = " "
+   var numberOfShapes = 0
+    var color = " "
+    var enabled = true
+    var isSelected = false
+    
+    
+    
+}
